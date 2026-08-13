ad865883769e002dac8a0bc0a7b67f43ef9db734
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2026-08-13 15:59:30 +0200

    fix: ci

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,12 @@
+import type { CreateConnectorFn } from "@wagmi/core"
 import { sliceWalletConnector } from "./provider/connector"
-import type { SliceWalletParameters } from "./types"
+import type { SliceWalletParameters, SliceWalletProvider } from "./types"
 
 export const sliceWalletConnectorId = "slice-wallet"
 
-export const sliceWallet = (parameters: SliceWalletParameters = {}) =>
+export const sliceWallet = (
+  parameters: SliceWalletParameters = {}
+): CreateConnectorFn<SliceWalletProvider> =>
   sliceWalletConnector({ announce: false, ...parameters })
 
 export * from "./wagmi/permissionActions"
